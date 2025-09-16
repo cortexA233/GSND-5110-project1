5695a002020265315167dee502defc450a7576d7
cortexA233 <cortexA233@outlook.com>
2025-09-16 15:54:14 -0400

    add more config

--- GSND_5110_repo_1/Assets/Scripts/SO_scripts/SO_GameConfig.cs
+++ GSND_5110_repo_1/Assets/Scripts/SO_scripts/SO_GameConfig.cs
@@ -6,4 +6,13 @@ public class SO_GameConfig : ScriptableObject
 {
     [Header("countdown time / 倒计时时间")]
     public int countDownTime;
+
+    [Header("minigame score to time ratio / 小游戏分数换算时间比例")]
+    public float scoreToExtraTimeRatio;
+
+    [Header("minigame time / 小游戏持续时间")]
+    public float minigameCountdownTime;
+
+    [Header("minigame frequency / 小游戏触发频率")]
+    public float minigameTriggerFrequency;
 }
